da16cc9e2b68cf76c3bbdb9a2bd85ebf98765a1d
ILYA NIKOLAENKO <dad.siberian@gmail.com>
2026-01-19 12:38:28 +0300

    Enhance FastAPI initialization with title, description, and custom documentation URLs

--- src/main.py
+++ src/main.py
@@ -8,6 +8,11 @@ from core import config
 logging.config.dictConfig(config.LOGGING_CONFIG)
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Organizations API",
+    description="API приложение для справочника Организаций, Зданий, Деятельности.",
+    docs_url="/docs",  # Swagger UI
+    redoc_url="/redoc",  # ReDoc
+)
 
 app.include_router(organizations.router)
