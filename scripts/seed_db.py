e4201ca0b3adb2d7ed624852eb027799b020fdba
ILYA NIKOLAENKO <dad.siberian@gmail.com>
2026-03-18 14:44:19 +0300

    Refactor seed_db.py: optimize database seeding process by using async session

--- scripts/seed_db.py
+++ scripts/seed_db.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 from pathlib import Path
 
+from sqlalchemy import insert
 from src.core.database import async_session
 from src.models import (
     Activity,
@@ -16,33 +17,33 @@ DATA_FILE = Path(__file__).parent.parent / "data/test_data.json"
 
 async def seed():
     async with async_session() as session:
-        with open(DATA_FILE, "r", encoding="utf-8") as data_file:
-            data = json.load(data_file)
+        async with session.begin():
+            with open(DATA_FILE, encoding="utf-8") as data_file:
+                data = json.load(data_file)
 
-        # Buildings
-        for build in data["buildings"]:
-            session.add(Building(**build))
+            # Buildings
+            for build in data["buildings"]:
+                session.add(Building(**build))
 
-        # Organizations
-        for organization in data["organizations"]:
-            session.add(Organization(**organization))
+            # Organizations
+            for organization in data["organizations"]:
+                session.add(Organization(**organization))
 
-        # Phones
-        for phone in data["phones"]:
-            session.add(Phone(**phone))
+            # Phones
+            for phone in data["phones"]:
+                session.add(Phone(**phone))
 
-        # Activities
-        for activity in data["activities"]:
-            session.add(await Activity.create(session, **activity))
+            # Activities
+            for activity in data["activities"]:
+                session.add(await Activity.create(session, **activity))
 
-        await session.commit()
+            await session.flush()
 
-        # Organization <-> Activity (association table)
-        conn = await session.connection()
-        for link in data["organization_activity"]:
-            await conn.execute(organization_activity.insert().values(**link))
-
-        await session.commit()
+            # Organization <-> Activity (association table)
+            await session.execute(
+                insert(organization_activity),
+                data["organization_activity"],
+            )
 
 
 if __name__ == "__main__":
